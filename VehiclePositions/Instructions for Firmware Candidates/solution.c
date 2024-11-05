5874142ac3e63130beae26d117659f12fb3e1145
KaraboMo <karabo.molemane@adaptit.com>
2024-11-05 15:04:51 +0200

    Correctly reads the file

--- VehiclePositions/Instructions for Firmware Candidates/solution.c
+++ VehiclePositions/Instructions for Firmware Candidates/solution.c
@@ -1,37 +1,72 @@
-/*
- * Use your preferred naming convention
- * Add comments to help others to understand your code and to document assumptions
- * You are welcome to add additional files if required
- */
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 typedef struct
 {
-    int position;
-    double latitude;
-    double longitude;
-} Position;
+    int32_t VehicleId;
+    char VehicleRegistration[256]; // Assuming max length of 256 for simplicity
+    float Latitude;
+    float Longitude;
+    uint64_t RecordedTimeUTC;
+} VehicleData;
+
+size_t read_vehicle_data(const char *filename, VehicleData *vehicles, size_t max_entries)
+{
+    FILE *file = fopen(filename, "rb");
+    if (!file)
+    {
+        perror("Failed to open file");
+        return 0;
+    }
+
+    size_t index = 0;
+    while (index < max_entries && fread(&vehicles[index].VehicleId, sizeof(int32_t), 1, file) == 1)
+    {
+        // Read VehicleRegistration
+        int i = 0;
+        while (i < 256)
+        {
+            fread(&vehicles[index].VehicleRegistration[i], sizeof(char), 1, file);
+            if (vehicles[index].VehicleRegistration[i] == '\0')
+                break;
+            i++;
+        }
+
+        // Read Latitude
+        fread(&vehicles[index].Latitude, sizeof(float), 1, file);
+
+        // Read Longitude
+        fread(&vehicles[index].Longitude, sizeof(float), 1, file);
+
+        // Read RecordedTimeUTC
+        fread(&vehicles[index].RecordedTimeUTC, sizeof(uint64_t), 1, file);
+
+        index++;
+    }
+
+    fclose(file);
+    return index;
+}
 
 int main()
 {
-    // 10 pre-defined co-ordinates or positions 
-    Position positions[10] = {
-        {1, 34.544909, -102.100843},
-        {2, 32.345544, -99.123124},
-        {3, 33.234235, -100.214124},
-        {4, 35.195739, -95.348899},
-        {5, 31.895839, -97.789573},
-        {6, 32.895839, -101.789573},
-        {7, 34.115839, -100.225732},
-        {8, 32.335839, -99.992232},
-        {9, 33.535339, -94.792232},
-        {10, 32.234235, -100.222222}};
-
-    for (int i = 0; i < 10; i++)
+    size_t max_entries = 2000000;
+    VehicleData *vehicles = malloc(max_entries * sizeof(VehicleData));
+    if (!vehicles)
     {
-        printf("Position %d: Latitude = %f, Longitude = %f\n", positions[i].position, positions[i].latitude, positions[i].longitude);
+        perror("Failed to allocate memory");
+        return 1;
     }
 
+    size_t count = read_vehicle_data("../VehiclePositions_DataFile/VehiclePositions.dat", vehicles, max_entries);
+
+    for (size_t i = 0; i < count && i < 20; i++)
+    {
+        printf("VehicleId: %d, VehicleRegistration: %s, Latitude: %f, Longitude: %f, RecordedTimeUTC: %llu\n",
+               vehicles[i].VehicleId, vehicles[i].VehicleRegistration, vehicles[i].Latitude, vehicles[i].Longitude, vehicles[i].RecordedTimeUTC);
+    }
+
+    free(vehicles);
     return 0;
 }
